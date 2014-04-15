e63c3449dd236b8b32d1970237622d5dcc93a3d0
nguyen.vo <nguyen.vo@konektasoft.com>
2014-04-15 14:32:15 +0700

    AutoLayout

---
+++ AutoLayout/AutoLayout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Chi nguyen Vo on 4/15/14.
+//  Copyright (c) 2014 Chi nguyen Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AutoLayout/AutoLayoutTests/AutoLayoutTests.m
@@ -0,0 +1,34 @@
+//
+//  AutoLayoutTests.m
+//  AutoLayoutTests
+//
+//  Created by Chi nguyen Vo on 4/15/14.
+//  Copyright (c) 2014 Chi nguyen Vo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoLayoutTests : XCTestCase
+
+@end
+
+@implementation AutoLayoutTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
